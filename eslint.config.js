3b421528883c477109fecf7d92977cc597d9d3ec
Lemonadeccc <zwjhb12@163.com>
2025-11-09 15:37:06 +0800

    feat: ✨  add commitlint & husky & cz & prettier.

--- eslint.config.js
+++ eslint.config.js
@@ -1,78 +1,75 @@
-const eslint = require("@eslint/js");
-const globals = require("globals");
-const reactHooks = require("eslint-plugin-react-hooks");
-const reactRefresh = require("eslint-plugin-react-refresh");
-const eslintPrettier = require("eslint-plugin-prettier");
-const importSort = require("eslint-plugin-simple-import-sort");
+const eslint = require('@eslint/js')
+const globals = require('globals')
+const reactHooks = require('eslint-plugin-react-hooks')
+const reactRefresh = require('eslint-plugin-react-refresh')
+const eslintPrettier = require('eslint-plugin-prettier')
+const importSort = require('eslint-plugin-simple-import-sort')
 
-const tseslint = require("typescript-eslint");
+const tseslint = require('typescript-eslint')
 
 const ignores = [
-  "dist",
-  "build",
-  "**/*.js",
-  "**/*.mjs",
-  "**/*.d.ts",
-  "eslint.config.js",
-  "commitlint.config.js",
-  "apps/frontend/monitor/src/components/ui/**/*",
-  "packages/browser/src/**/*",
-  "packages/browser-utils/src/metrics/**/*",
-  ".devcontainer/**/*",
-];
+    'dist',
+    'build',
+    '**/*.js',
+    '**/*.mjs',
+    '**/*.d.ts',
+    'eslint.config.js',
+    'commitlint.config.js',
+    'apps/frontend/monitor/src/components/ui/**/*',
+    'packages/browser/src/**/*',
+    'packages/browser-utils/src/metrics/**/*',
+    '.devcontainer/**/*',
+]
 
 const frontendMonitorConfig = {
-  files: ["apps/frontend/monitor/**/*.{ts,tsx}"],
-  ignores: ["apps/frontend/monitor/src/components/ui/**/*", "**/*.d.ts"],
-  languageOptions: {
-    ecmaVersion: 2020,
-    globals: globals.browser,
-  },
-  plugins: {
-    "react-hooks": reactHooks,
-    "react-refresh": reactRefresh,
-  },
-  rules: {
-    ...reactHooks.configs.recommended.rules,
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true },
-    ],
-    "no-console": "error",
-  },
-};
-
-const backendMonitorConfig = {
-  files: ["apps/backend/**/*.ts"],
-  languageOptions: {
-    globals: {
-      ...globals.node,
-      ...globals.jest,
+    files: ['apps/frontend/monitor/**/*.{ts,tsx}'],
+    ignores: ['apps/frontend/monitor/src/components/ui/**/*', '**/*.d.ts'],
+    languageOptions: {
+        ecmaVersion: 2020,
+        globals: globals.browser,
     },
-    parser: tseslint.parser,
-  },
-  rules: {
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/explicit-module-boundary-types": "off",
-    "@typescript-eslint/interface-name-prefix": "off",
-    "@typescript-eslint/no-explicit-any": "off",
-    "no-console": "error",
-  },
-};
-
-module.exports = tseslint.config(
-  {
-    ignores,
-    extends: [eslint.configs.recommended, ...tseslint.configs.recommended],
     plugins: {
-      prettier: eslintPrettier,
-      "simple-import-sort": importSort,
+        'react-hooks': reactHooks,
+        'react-refresh': reactRefresh,
     },
     rules: {
-      "prettier/prettier": "error",
-      "simple-import-sort/imports": "error",
+        ...reactHooks.configs.recommended.rules,
+        'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+        'no-console': 'error',
     },
-  },
-  frontendMonitorConfig,
-  backendMonitorConfig
-);
+}
+
+const backendMonitorConfig = {
+    files: ['apps/backend/**/*.ts'],
+    languageOptions: {
+        globals: {
+            ...globals.node,
+            ...globals.jest,
+        },
+        parser: tseslint.parser,
+    },
+    rules: {
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/interface-name-prefix': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        'no-console': 'error',
+    },
+}
+
+module.exports = tseslint.config(
+    {
+        ignores,
+        extends: [eslint.configs.recommended, ...tseslint.configs.recommended],
+        plugins: {
+            prettier: eslintPrettier,
+            'simple-import-sort': importSort,
+        },
+        rules: {
+            'prettier/prettier': 'error',
+            'simple-import-sort/imports': 'error',
+        },
+    },
+    frontendMonitorConfig,
+    backendMonitorConfig
+)
