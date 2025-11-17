fe8e6711a1f22103643621bf2891bc2b46bb6d63
Lemonadeccc <zwjhb12@163.com>
2025-11-17 16:47:30 +0800

    feat(backend): ✨  add error filter for backend repo.

--- apps/backend/monitor/src/main.ts
+++ apps/backend/monitor/src/main.ts
@@ -1,15 +1,42 @@
-import { NestFactory } from '@nestjs/core'
+import { ConfigService } from '@nestjs/config'
+import { HttpAdapterHost, NestFactory } from '@nestjs/core'
 
 import { AppModule } from './app.module'
+import { AllExceptionFilter } from './common/filters/all-exception.filter'
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, {
         // logger: false,
         // logger: ['error', 'warn'],
     })
+    const configService = app.get(ConfigService)
+
+    const cors = configService.get('CORS', true)
+    if (cors === 'true') {
+        app.enableCors()
+    }
+
+    // const versionStr = configService.get<string>('VERSION', '1')
+    // let version = [versionStr]
+    // if (versionStr.indexOf(',')) {
+    //     version = versionStr.split(',')
+    // }
+    // app.enableVersioning({
+    //     type: VersioningType.URI,
+    //     defaultVersion: [...version],
+    // })
+
+    // const prefix = configService.get('PREFIX', '/api')
+    // app.setGlobalPrefix(prefix)
+
+    const errorFilterFlag = configService.get<string>('ERROR_FILTER')
+    if (errorFilterFlag) {
+        const httpAdapter = app.get(HttpAdapterHost)
+        app.useGlobalFilters(new AllExceptionFilter(httpAdapter))
+    }
     // const configService = app.get(ConfigService)
     // const port = configService.get<number>('PORT', 3000)
     // await app.listen(port)
-    await app.listen(process.env.PORT ?? 3000)
+    await app.listen(3000)
 }
 bootstrap()
