51ae115cd7b6b9aaa1bd8a43bb3f25f0e01a07a0
Lemonadeccc <zwjhb12@163.com>
2025-11-27 17:34:35 +0800

    fix(backend): 🐛  add argon2 to crypto password.

--- apps/backend/monitor/src/admin/admin.service.ts
+++ apps/backend/monitor/src/admin/admin.service.ts
@@ -1,5 +1,6 @@
-import { HttpException, Injectable } from '@nestjs/common'
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
+import { hash, verify } from 'argon2'
 import { Repository } from 'typeorm'
 
 import { AdminEntity } from './admin.entity'
@@ -13,8 +14,18 @@ export class AdminService {
 
     async validateUser(username: string, pass: string): Promise<any> {
         const admin = await this.adminRepository.findOne({
-            where: { username, password: pass },
+            where: { username },
         })
+
+        if (!admin) {
+            throw new HttpException({ message: 'User not found', error: 'INVALID_USERNAME' }, HttpStatus.BAD_REQUEST)
+        }
+
+        const isValid = await verify(admin.password, pass)
+        if (!isValid) {
+            throw new HttpException({ message: 'Invalid password', error: 'INVALID_PASSWORD' }, HttpStatus.BAD_REQUEST)
+        }
+
         return admin
     }
 
@@ -23,7 +34,7 @@ export class AdminService {
             where: { username: body.username },
         })
         if (adminIsExist) {
-            throw new HttpException({ message: 'user is existed', error: 'user is existed' }, 400)
+            throw new HttpException({ message: 'Username already exists', error: 'USERNAME_EXISTS' }, HttpStatus.CONFLICT)
         }
 
         if (body.email) {
@@ -31,11 +42,14 @@ export class AdminService {
                 where: { email: body.email },
             })
             if (emailIsExist) {
-                throw new HttpException({ message: 'email is existed', error: 'email is existed' }, 400)
+                throw new HttpException({ message: 'Email already exists', error: 'EMAIL_EXISTS' }, HttpStatus.CONFLICT)
             }
         }
 
-        const admin = await this.adminRepository.create(body)
+        const admin = await this.adminRepository.create({
+            ...body,
+            password: await hash(body.password),
+        })
         await this.adminRepository.save(admin)
         return admin
     }
