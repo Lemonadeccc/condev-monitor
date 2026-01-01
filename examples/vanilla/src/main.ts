3e84a8c915ec133026a2bb011daed09f69024d57
Lemonadeccc <zwjhb12@163.com>
2026-01-01 13:39:46 +0800

    revert: :rewind: change deploy/yml in devcontianer

--- examples/vanilla/src/main.ts
+++ examples/vanilla/src/main.ts
@@ -16,9 +16,14 @@ init({
 //     monitoring.reportEvent({ type: 'click' })
 //     monitoring.reportMessage('event')
 // })
-myFn()
+// myFn()
 
 // Promise.reject('test')
+
+// 定义会抛出错误的函数，用于测试监控SDK
+function myFn() {
+    throw new Error('This is a test error triggered by button click')
+}
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
@@ -31,6 +36,7 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <h1>Vite + TypeScript</h1>
     <div class="card">
       <button id="counter" type="button"></button>
+      <button id="error-btn" type="button" style="margin-left: 10px; background-color: #f44336;">触发错误</button>
     </div>
     <p class="read-the-docs">
       Click on the Vite and TypeScript logos to learn more
@@ -39,3 +45,7 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 `
 
 setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+
+document.querySelector<HTMLButtonElement>('#error-btn')!.addEventListener('click', () => {
+    myFn()
+})
